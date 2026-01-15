f228c0df14b3730baa115be083e209229463776c
Akinyele Thompson <akinyelethompson@gmail.com>
2026-01-15 08:37:56 -0500

    fix dark mode; updated readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
     }, [])
 
     return (
-        <main className="min-h-screen bg-gray-50">
+        <main className="min-h-screen bg-gray-50 dark:bg-gray-900">
             <Toaster position="top-right" />
 
             <header className="bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 sticky top-0 z-10">
